a34dbb61e2bac0b1e75856c06a0ae709528fb033
serenityFX <victoryagudaev@gmail.com>
2016-02-28 12:26:23 +0300

    solce errors

--- server.py
+++ server.py
@@ -3,12 +3,12 @@ import socket
 
 class EchoHandler(asyncore.dispatcher_with_send):
 
-	def handle_read(self)
+	def handle_read(self):
 		data = self.recv(1024)
 
-		if data == 'close'
+		if data == 'close':
 			self.close()
-		else
+		else:
 			self.send(data)
 
 class EchoServer(asyncore.dispatcher):
@@ -17,12 +17,12 @@ class EchoServer(asyncore.dispatcher):
 		asyncore.dispatcher.__init__(self)
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.set_reuse_addr()
-		self.bind(host,port)
+		self.bind((host,port))
 		self.listen(15)
 
 	def handle_accept(self):
 		pair = self.accept()
-		if pair is note None:
+		if pair is not None:
 			sock,addr = pair
 			print('Incoming connection from %s' % repr(addr))
 			handler = EchoHandler(sock)
